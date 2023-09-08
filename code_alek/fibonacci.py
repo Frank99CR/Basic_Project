1b7deae84d17bee52acceb6667efbfd1486bfbde
Francisco Ramirez <44051293+Frank99CR@users.noreply.github.com>
2023-09-07 23:28:18 -0600

    Delete code_alek directory

--- code_alek/fibonacci.py
+++
@@ -1,9 +0,0 @@
-def fibonacci_sequence(num1, num2, counter): 
-    if counter <= 10:
-     print(num1)
-     fibonacci_sum = num1 + num2
-     fibonacci_sequence(fibonacci_sum, num1, counter + 1)
-        
-        
-        
-fibonacci_sequence(1, 0, 0)
