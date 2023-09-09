3a1e413a1b153fdf0c8e0095e8a90240f467ee95
Francisco Ramirez <pacoramirezcr@gmail.com>
2023-09-08 20:24:38 -0600

    Submit project

--- funciones/Project_1.py
+++ funciones/Project_1.py
@@ -3,43 +3,52 @@ list_of_scores = []
 top_score_student = []
 
 def register_student():
-  name = input("Please enter your name: ")
-  last_name = input("Please enter your last name: ")
-  score_per_student = float(input("Please enter your score: "))
-  final_name = name +" "+ last_name
+  student_name = input("Please enter your name: ")
+  student_last_name = input("Please enter your last name: ")
+  student_score = float(input("Please enter your score: "))
+  final_name = student_name +" "+ student_last_name
   list_of_students.append(final_name)
-  list_of_scores.append(score_per_student)
+  list_of_scores.append(student_score)
 
   print("Registro exitoso")
   
   menu()
 
 def get_list_of_students(list_of_students, list_of_scores):
-  for student in list_of_students:
-    pass
-    for score in list_of_scores:
-      pass
-    print(f"Name:{student}: Score:{score}")
+  
+    # for student in list_of_students: // # not working properly
+    #  for score in list_of_scores:
+    #   print(f"Name: {student}, Score: {score}")
 
-  menu()  
+    for student in list_of_students:
+      print(f"Name: {student}")
+    for score in list_of_scores:
+      print(f"Score: {score}")  
+
+
+    menu()  
+    
+  
+  
 
 def get_top_scores(list_of_scores):
-  top_three_student = []
+  top_three_scores = []
   for best_score in list_of_scores:
-    top_three_student.append(best_score)
-  top_three_student.sort(reverse=True)    
-  print(top_three_student)
+    top_three_scores.append(best_score)
+  top_three_scores.sort(reverse=True)    #
+  print(top_three_scores)
   menu()
 
 
 def get_average_of_scores(list_of_scores):
   sum_of_scores = 0
-  for scores in list_of_scores:
-    sum_of_scores = sum_of_scores + scores
+  for score in list_of_scores:
+    sum_of_scores = sum_of_scores + score
 
   final_average = sum_of_scores / len(list_of_scores)  
 
-  print(round(final_average, 2))
+  #print(round(final_average, 2))
+  print(f"The average of all scores is: {round(final_average, 2)}")
   menu()
 
 
