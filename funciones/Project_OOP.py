edc45e36ae74162bbc96a3dbf704b4d0a3b8a7fa
Francisco Ramirez <pacoramirezcr@gmail.com>
2023-09-15 23:01:51 -0600

    Obj student and dict implemented

--- funciones/Project_OOP.py
+++ funciones/Project_OOP.py
@@ -1,50 +1,69 @@
-list_of_students = []
-list_of_scores = []
-top_score_student = []
+import operator
+class Student:
+  name : str
+  last_name : str
+  score : int
 
-def register_student():
+  def __init__(self, name, last_name, score):
+   self.name = name
+   self.last_name = last_name
+   self.score = score
+
+  def print_name(self):
+   print(self.name)  
+
+
+def entry_point():
+  list_of_students = []
+  menu(list_of_students)
+
+
+def register_student(list_of_students):
   name = input("Please enter your name: ")
   last_name = input("Please enter your last name: ")
-  score_per_student = float(input("Please enter your score: "))
-  final_name = name +" "+ last_name
-  list_of_students.append(final_name)
-  list_of_scores.append(score_per_student)
-
+  score = float(input("Please enter your score: "))
+  student1 = Student(name, last_name, score)
+  student_dict = {
+    "Name" : student1.name,
+    "Last Name" : student1.last_name,
+    "Score" : student1.score
+  }
+  list_of_students.append(student_dict)
   print("Registro exitoso")
   
-  menu()
+  menu(list_of_students)
 
-def get_list_of_students(list_of_students, list_of_scores):
+def get_list_of_students(list_of_students):
   for student in list_of_students:
-    for score in list_of_scores:
-      print(f"Name:{student}: Score:{score}")
+    print(student)
+  menu(list_of_students)  
 
-  menu()  
+def get_top_scores(list_of_students):
+    
+    print(sorted(list_of_students[0:3], key=operator.itemgetter('Score'), reverse=True))
+    
+    menu(list_of_students)
 
-def get_top_scores(list_of_scores):
-  top_three_student = []
-  for best_score in list_of_scores:
-    top_three_student.append(best_score)
-  top_three_student.sort(reverse=True)    
-  print(top_three_student)
-  menu()
+  
 
 
-def get_average_of_scores(list_of_scores):
-  sum_of_scores = 0
-  for scores in list_of_scores:
-    sum_of_scores = sum_of_scores + scores
+def get_average_of_scores(list_of_students):
 
-  final_average = sum_of_scores / len(list_of_scores)  
+  list_of_scores = []
+  sum = 0
+  
+  for x in list_of_students:
+    score = x.get("Score")
+    list_of_scores.append(int(score))
+  for score in list_of_scores:
+    sum = sum + score
 
-  print(round(final_average, 2))
-  menu()
+  final_average = sum / len(list_of_scores) 
+  print(f"Average Score: {round(final_average, 2)}")   
+  menu(list_of_students)
 
 
-
-
-
-def menu():
+def menu(list_of_students):
 
   print("""    
     --------------------------------------------------
@@ -61,23 +80,23 @@ def menu():
   user_input = input("Digite una opcion del menu: ")
 
   if user_input == "1":
-    register_student()
+    register_student(list_of_students)
   elif user_input == "2":
-    get_list_of_students(list_of_students, list_of_scores)
+    get_list_of_students(list_of_students)
   elif user_input == "3":
-    get_top_scores(list_of_scores)  
+    get_top_scores(list_of_students)  
   elif user_input == "4":
-    get_average_of_scores(list_of_scores)
+    get_average_of_scores(list_of_students)
   elif user_input == "5": 
     print("Gracias por usar el sistema")
   else:
     print("Digite la opcion correcta ")
-    menu()  
+    menu(list_of_students)  
     
   
 
 
-menu()
+entry_point()
 
 
 
