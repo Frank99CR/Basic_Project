55314053cb357949261180f5814164657d74f271
Francisco Ramirez <pacoramirezcr@gmail.com>
2023-09-16 11:30:05 -0600

    new files added to OOP Folder

---
+++ OOP/OOP_Herencia_Multiple.py
@@ -0,0 +1,8 @@
+# Posible usos:
+# Compartir funcionalidad común: 
+# Interfaces y mixins:
+# Composición de clases:
+# Resolución de problemas de diseño:
+# Herencia de bibliotecas y frameworks:
+
+
