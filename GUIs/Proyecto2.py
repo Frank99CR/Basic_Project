f54e49d3fcd30a5bf7408e2afa47df54ef02946c
Francisco Ramirez <pacoramirezcr@gmail.com>
2024-03-07 14:09:21 -0600

    HTML files and Project #2

--- GUIs/Proyecto2.py
+++ GUIs/Proyecto2.py
@@ -1,110 +1,92 @@
 import PySimpleGUI as sg
+import json
 
 sg.set_options(font=("Arial Bond", 20))
-
-def main_window():
-
-    layout = [
-    [sg.Column([[sg.Text("Bienvenido a mis finanzas", size=(20, 1))]], justification="center")],
-    [sg.Column([[sg.Button("Ingresos", size=(15,1))]], justification="center")],
-    [sg.Column([[sg.Button("Gastos", size=(15,1))]], justification="center")],
-    [sg.Column([[sg.Button("Salir", size=(15,1))]], justification="center")]
-   ]
-    return sg.Window("Mis finanzas", layout, size=(450,250))
-
-def income_window():
-  top_row_for_description_table = ['Titulo']
-  rows_for_descriptions = []
-  top_row_for_category_table = ['Categoria']
-  rows_for_categories = []
-  top_row_for_amount_table = ['Monto']
-  rows_for_amounts = []
-  dict_of_income = []
-
-  layout = [ 
-    [sg.Column([[sg.Table(values=rows_for_categories, headings=top_row_for_category_table, auto_size_columns=False, display_row_numbers=False, justification='left', key='-TABLE_CATEGORY-', selected_row_colors=('red','yellow'), enable_events=True, expand_x=False, expand_y=True, enable_click_events=True)]], justification="left"),
-     sg.Table(values=rows_for_descriptions, headings=top_row_for_description_table, auto_size_columns=True, display_row_numbers=False, justification='center', key='-TABLE_DESCRIPTION-', selected_row_colors=('red','yellow'), enable_events=True, expand_x=True, expand_y=False, enable_click_events=True),
-     sg.Table(values=rows_for_amounts, headings=top_row_for_amount_table, auto_size_columns=True, display_row_numbers=False, justification='center', key='-TABLE_AMOUNT-', selected_row_colors=('red','yellow'), enable_events=True, expand_x=True, expand_y=False, enable_click_events=True),
-     ],
-    #[sg.Table(values=rows, headings=top_row, auto_size_columns=True, display_row_numbers=False, justification='center', key='-TABLE-', selected_row_colors=('red','yellow'), enable_events=True, expand_x=True, expand_y=False, enable_click_events=True)],
-    # [sg.Table(values=rows2, headings=top_row2, auto_size_columns=False, display_row_numbers=False, justification='left', key='-TABLE-', selected_row_colors=('red','yellow'), enable_events=True, expand_x=False, expand_y=True, enable_click_events=True)],
-    [sg.Text("Ingrese la categoria: ")],
-    [sg.Input(key='-Categoria-')],
-    [sg.Text("Ingrese la Descripcion: ")],
-    [sg.Input(key='-Titulo-')],
-    [sg.Text("Ingrese el monto: ")],
-    [sg.Input(key='-Monto-')],
-    [sg.Button('Agregar categoria', key="-submit_category-")],
-    [sg.Button('Agregar descripcion', key="-submit_description-", disabled=True, tooltip='Primero debes ingresar una categoria')],
-    [sg.Button('Agregar monto', key="-submit_amount-", disabled=True, tooltip='Primero debes ingresar una descripcion')],
-    [sg.Button("Limpiar")],
-    [sg.Button("Exportar", key="-EXPORT-")],
-    # [sg.Button("Importar", key="-IMPORT-")]
-    [sg.Button("Salir", key="-EXIT-")]
-    ]
-  return sg.Window("Ingresos", layout)
+# Diseño de las pestañas
+tab1_layout = [
+    [sg.Text('Registre las categorias')],
+    [sg.InputText('', key='-categoria-')],
+    [sg.Button('Ingresar', key="-submit-")]
+]
+rows = []
+toprow = ["Categoria", "Descripcion", "Monto"]
+list_of_categories = []
+financial_data = []
+financial_dict= {}
 
 
-def expense_window():
-    
-  top_row_for_description_table = ['Titulo']
-  rows_for_descriptions = []
-  top_row_for_category_table = ['Categoria']
-  rows_for_categories = []
-  top_row_for_amount_table = ['Monto']
-  rows_for_amounts = []
-  dict_of_expenses = []
+def export_data_to_json(path):
 
-  layout = [ 
-    [sg.Column([[sg.Table(values=rows_for_categories, headings=top_row_for_category_table, auto_size_columns=False, display_row_numbers=False, justification='left', key='-TABLE_CATEGORY-', selected_row_colors=('red','yellow'), enable_events=True, expand_x=False, expand_y=True, enable_click_events=True)]], justification="left"),
-     sg.Table(values=rows_for_descriptions, headings=top_row_for_description_table, auto_size_columns=True, display_row_numbers=False, justification='center', key='-TABLE_DESCRIPTION-', selected_row_colors=('red','yellow'), enable_events=True, expand_x=True, expand_y=False, enable_click_events=True),
-     sg.Table(values=rows_for_amounts, headings=top_row_for_amount_table, auto_size_columns=True, display_row_numbers=False, justification='center', key='-TABLE_AMOUNT-', selected_row_colors=('red','yellow'), enable_events=True, expand_x=True, expand_y=False, enable_click_events=True),
-     ],
-    #[sg.Table(values=rows, headings=top_row, auto_size_columns=True, display_row_numbers=False, justification='center', key='-TABLE-', selected_row_colors=('red','yellow'), enable_events=True, expand_x=True, expand_y=False, enable_click_events=True)],
-    # [sg.Table(values=rows2, headings=top_row2, auto_size_columns=False, display_row_numbers=False, justification='left', key='-TABLE-', selected_row_colors=('red','yellow'), enable_events=True, expand_x=False, expand_y=True, enable_click_events=True)],
-    [sg.Text("Ingrese la categoria: ")],
-    [sg.Input(key='-Categoria-')],
-    [sg.Text("Ingrese la Descripcion: ")],
-    [sg.Input(key='-Titulo-')],
-    [sg.Text("Ingrese el monto: ")],
-    [sg.Input(key='-Monto-')],
-    [sg.Button('Agregar categoria', key="-submit_category-")],
-    [sg.Button('Agregar descripcion', key="-submit_description-", disabled=True, tooltip='Primero debes ingresar una categoria')],
-    [sg.Button('Agregar monto', key="-submit_amount-", disabled=True, tooltip='Primero debes ingresar una descripcion')],
-    [sg.Button("Limpiar")],
-    [sg.Button("Exportar", key="-EXPORT-")],
-    # [sg.Button("Importar", key="-IMPORT-")]
-    ]
-  return sg.Window("Gastos", layout)
+  income_data_converted_to_json = json.dumps(financial_data, indent=4)
+  with open (path, 'w') as file:
+   file.writelines(income_data_converted_to_json)
+  [sg.popup("Archivo Exportado exitosamente")]
 
 
 
-# current_window = main_window()
+tab2_layout = [ 
+        [sg.Table(values=rows, headings=toprow, auto_size_columns=True, display_row_numbers=False, justification='center', key='-TABLE-', selected_row_colors=('black on white'), enable_events=True, expand_x=True, expand_y=True, enable_click_events=True)],
+        [sg.Combo(list_of_categories, key='-combo-', default_value="Seleccione una opcion", size=(20, 1))],
+        [sg.InputText('Ingrese el titulo', key='-titulo-')],
+        [sg.InputText('Ingrese el monto', key='-monto-')],
+        [sg.Button('Ingresar', key='-ADD-')],
+        [sg.Button('Limpiar', key='-CLEAR-')],
+        [sg.Button('Exportar', key='-EXPORT-')]
 
-def main_window_cycle():
- 
- current_window = main_window()
- while True:
-  event, values = current_window.read()
+]
 
-  if event == sg.WIN_CLOSED:
-    break
-  elif event == "Ingresos":
-    current_window.hide()
-    current_window = income_window()
-  elif event == "Gastos":
-    current_window.hide()
-    current_window = expense_window()
-  elif event == "Salir":
-   break
- 
- current_window.close()
+#Diseño principal
+layout = [
+    [sg.TabGroup([
+        [sg.Tab('Categorias', tab1_layout)],
+        [sg.Tab('Movimientos', tab2_layout)]
+    ])],
+    [sg.Button('Salir')]
+]
 
-
-
-
-
-main_window_cycle()
+# Crear la ventana
+window = sg.Window('Mis Finanzas', layout)
 
 
 
+# Loop principal
+while True:
+    event, values = window.read()
+
+    if event in (sg.WIN_CLOSED, 'Salir'):
+        break
+    elif event == '-submit-':
+        new_category = values['-categoria-']
+        if new_category in list_of_categories:
+            sg.popup("La categoria ya existe")
+        else:
+            list_of_categories.append(new_category)
+            sg.popup("Categoria registrada")
+            print(list_of_categories)
+            window['-combo-'].update(values=list_of_categories)
+            window['-categoria-'].update("")
+    elif event == '-CLEAR-':
+      window['-combo-'].update("")
+      window['-titulo-'].update("")
+      window['-monto-'].update("")    
+    elif event == '-ADD-':
+      new_row = [values['-combo-'], values['-titulo-'], values['-monto-']]
+      rows.append(new_row)
+      if values ['-combo-'] not in list_of_categories:
+         sg.popup("Categoria no registrada")
+      else: 
+        window['-TABLE-'].update(values=rows)
+    elif event == '-EXPORT-':
+       
+       data_dict= {
+            "Category" : values['-combo-'],
+            "Description" : values['-titulo-'],
+            "Amount" : values['-monto-'],
+         }
+       financial_data.append(data_dict)
+       print(financial_data)
+       export_data_to_json("Archivos/Ingresos")
+          
+
+
+window.close()
